1e75e06578133891b5ca1e5901ca7b612023257a
taufort <taufort@ippon.fr>
2022-03-25 08:19:43 +0100

    feat(networking): add solution to module

--- 05_networking/vpc.tf
+++ 05_networking/vpc.tf
@@ -7,3 +7,48 @@ resource "aws_vpc" "main" {
     Name = "${var.project}-vpc"
   }
 }
+
+locals {
+  az_a = "${var.region}a"
+  az_b = "${var.region}b"
+  az_c = "${var.region}c"
+}
+
+resource "aws_subnet" "public_a" {
+  cidr_block              = "10.1.0.0/24"
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  availability_zone       = local.az_a
+}
+
+resource "aws_subnet" "public_b" {
+  cidr_block              = "10.1.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  availability_zone       = local.az_b
+}
+
+resource "aws_subnet" "public_c" {
+  cidr_block              = "10.1.2.0/24"
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  availability_zone       = local.az_c
+}
+
+resource "aws_subnet" "private_a" {
+  cidr_block        = "10.1.4.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = local.az_a
+}
+
+resource "aws_subnet" "private_b" {
+  cidr_block        = "10.1.5.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = local.az_b
+}
+
+resource "aws_subnet" "private_c" {
+  cidr_block        = "10.1.6.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = local.az_c
+}
