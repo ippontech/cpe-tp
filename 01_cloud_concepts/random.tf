09ace770d98d665fe8b5d6e9c6bdf6a0bca8d58b
Timothée Aufort <timothee.aufort@gmail.com>
2023-03-02 18:40:26 +0100

    feat: add cloud concepts module into the CPE TP (#12)

---
+++ 01_cloud_concepts/random.tf
@@ -0,0 +1,4 @@
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/integer
+# You need to create a random integer below this comment
+
+# You need to create a Terraform output of the result of your random integer below this comment
