ae87f21b0233da34785f03d2380530613dbbdba1
Yoan SIMIAND-COSSIN <127422619+ysimiandcossin@users.noreply.github.com>
2023-03-24 17:19:22 +0100

    fix(module 6): fix security group egress rule (#22)

--- 06_compute/modules/compute/0-ec2.tf
+++ 06_compute/modules/compute/0-ec2.tf
@@ -22,7 +22,5 @@ resource "aws_vpc_security_group_egress_rule" "ssh_security_group_egress_rule" {
   security_group_id = aws_security_group.ssh_security_group.id
 
   cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
   ip_protocol = "-1"
-  to_port     = 0
 }
